ca39f5ea1caa47d6ded1d60b5c071118977feeb0
Beyonder12 <fajri.illahi1211@gmail.com>
2023-10-12 04:21:31 +0700

    CRUD welcome implementation

--- malicfin/main.py
+++ malicfin/main.py
@@ -1,21 +1,31 @@
 import os
 
 if __name__ == "__main__":
-    operation_system = os.name
-
-    match operation_system:
-        case "posix": os.system('clear')
-        case 'nt': os.system('cls')
-
-    print('WELCOME TO THE APPLICATION')
-    print('DATABASE OF LIBARY')
-    print('===========================')
-
     while(True):
+        operation_system = os.name
+        match operation_system:
+            case "posix": os.system('clear')
+            case 'nt': os.system('cls')
+
+        print('WELCOME TO THE APPLICATION')
+        print('DATABASE OF LIBARY')
+        print('===========================')
         print(f'1. Create Data')
         print(f'2. Read Data')
         print(f'3. Update Data')
         print(f'4. Delete Data')
 
-        user_option = input('Input your option: ')
+        user_option = input( '\nInput your option: ')
+        print('\n===========================\n')
+        match user_option:
+            case "1": print("Create Data")
+            case "2": print("Read Data")
+            case "3": print("Update Data")
+            case "4": print("Delete Data")
+
+        print('\n===========================\n')
+        is_done = input('Is it done?(yes/no/y/n)')
+        if is_done.lower() == 'y' or is_done == 'yes':
+            os.system('clear');break
+    print("\nProgram is Over. Thanks!\n")
 
