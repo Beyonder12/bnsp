bd9313025de90b9838c191f8a12d484ce19c2e5f
Beyonder12 <fajri.illahi1211@gmail.com>
2023-10-16 19:13:20 +0700

    After try 2

--- day12uk6reviewuk7erroexceptiondebugging/main.py
+++ day12uk6reviewuk7erroexceptiondebugging/main.py
@@ -2,18 +2,21 @@ import os
 
 
 while True:
-
-        os.system('clear')
+    try:
+        # os.system('clear')
         
         print(' Welcome to The Club')
         print('====================')
         print('1. Input your name. ')
         print('2. Exit.')
         option = input('Input your options: ')
-
         if option == '2':
             print('Thanks for coming!')
             break
         name = input('Name: ')
         print('\nHello, Mr./Mrs.', name + '\n')
         print('====================')
+        option/5
+    except:
+        pass
+    
